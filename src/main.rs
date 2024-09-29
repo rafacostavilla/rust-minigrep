76404141a71a1eb6652befb1c9d4df7349bf870d
rafacostavilla <rafaelcostavilla@gmail.com>
2024-09-29 17:28:30 +1300

    Refactor code: modularise code breaking down responsibilities into parsing and error handling code blocks

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,53 @@
+use std::error::Error;
 use std::{env, fs};
+use std::process;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+    
+    if let Err(e) = run(config){
+        println!("Application error: {e}");
+        process::exit(1);
+    } 
+}
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.filename)?;
+    
+    println!("With text:\n");
+    println!("----------------------------");
+    println!("{contents}");
+    println!("----------------------------");
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
+    Ok(())
+}
 
-    println!("With text:\n{contents}");
+struct Config{
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str>{
+        if args.len() < 3{
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Ok(
+        Config{
+            query,
+            filename,
+        })
+    }
 }
