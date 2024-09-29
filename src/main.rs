12ae553f7a0e3285ec10ca9946dc7fb29a32ce9d
rafacostavilla <rafaelcostavilla@gmail.com>
2024-09-29 19:04:54 +1300

    Separate the error handling and parsing from the binary crate using a new lib

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::error::Error;
-use std::{env, fs};
+use std::env;
 use std::process;
+
+use rust_minigrep::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,37 +18,4 @@ fn main() {
         println!("Application error: {e}");
         process::exit(1);
     } 
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.filename)?;
-    
-    println!("With text:\n");
-    println!("----------------------------");
-    println!("{contents}");
-    println!("----------------------------");
-
-    Ok(())
-}
-
-struct Config{
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str>{
-        if args.len() < 3{
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-    
-        Ok(
-        Config{
-            query,
-            filename,
-        })
-    }
 }
